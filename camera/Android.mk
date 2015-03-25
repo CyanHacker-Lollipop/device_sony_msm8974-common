CyanogenMod/android_device_sony_msm8974-common

move camera wrapper down a level from qcom-common
Change-Id: I788cb33fbf4043ae5133581a9cc2b728aab991cb

camera/Android.mk
@@ -1,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    CameraWrapper.cpp
+
+LOCAL_C_INCLUDES := \
+    system/media/camera/include
+
+LOCAL_SHARED_LIBRARIES := \
+    libhardware liblog libcamera_client libutils libcutils
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
+LOCAL_MODULE := camera.msm8974
+
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)

msm8974.mk
@@ -39,6 +39,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    camera.msm8974 \
     CameraWorkaround
 
 PRODUCT_PACKAGES += \

CyanHacker-Lollipop/device_sony_msm8974-common

camera/Android.mk
@@ -1,0 +1,19 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    CameraWrapper.cpp
+
+LOCAL_C_INCLUDES := \
+    system/media/camera/include
+
+LOCAL_SHARED_LIBRARIES := \
+    libhardware liblog libcamera_client libutils libcutils
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
+LOCAL_MODULE := camera.qcom
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)

msm8974.mk
@@ -39,6 +39,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    camera.qcom \
     CameraWorkaround
 
 PRODUCT_PACKAGES += \